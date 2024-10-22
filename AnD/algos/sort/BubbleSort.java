155750385f2f58b0bc0f7046d5740283d198cbb9
heitzlki <kirill.heitzler@gmail.com>
2024-10-22 02:34:23 +0200

    Finish BubbleSort

--- AnD/algos/sort/BubbleSort.java
+++ AnD/algos/sort/BubbleSort.java
@@ -6,11 +6,22 @@ import java.util.Arrays;
 public class BubbleSort {
   public static void main(String[] args) {
     int[] array = {1, 5, 4, 3, 2};
-    int[] sorted = sort(array);
-    System.out.println(Arrays.toString(array) + "-->" + sorted);
+    int[] sorted = sort(Arrays.copyOf(array, array.length));
+    System.out.println(Arrays.toString(array) + " --> " + Arrays.toString(sorted));
   }
 
   public static int[] sort(int[] array) {
+
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+
     return array;
   }
 }
