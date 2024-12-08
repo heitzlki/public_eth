8ab2ed23db3e9909be05d597bb265ef3f5d08940
heitzlki <kirill.heitzler@gmail.com>
2024-12-08 10:28:06 +0100

    play around with unicode values for string and lexographical order

---
+++ tmp/UnicodeValues.java
@@ -0,0 +1,40 @@
+package tmp;
+
+public class UnicodeValues {
+  public static void main(String[] args) {
+    String str = "abcdefghijklmnopqrstuvwxyz";
+    String str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String str3 = "0123456789";
+    String str4 = "!@#$%^&*()_+";
+
+    printUnicodeValues(str);
+
+
+    String one = "Hello";
+    String two = "World";
+    printCompareTo(one, two);
+  }
+
+  public static void printUnicodeValues(String str) {
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      int unicodeValue = (int) c;
+      System.out.println(c + " -> " + unicodeValue);
+    }
+  }
+
+  public static void printCompareTo(String one, String two) {
+    if (one != null && two != null) {
+      char oneChar = one.charAt(0);
+      int oneUnicode = (int) oneChar;
+
+      char twoChar = two.charAt(0);
+      int twoUnicode = (int) twoChar;
+      System.out.println(one + " : " + oneChar + " -> " + oneUnicode);
+      System.out.println(two + " : " + twoChar + " -> " + twoUnicode);
+
+
+      System.out.println(oneUnicode + " - " + twoUnicode + " = " + one.compareTo(two));
+    }
+  }
+}
